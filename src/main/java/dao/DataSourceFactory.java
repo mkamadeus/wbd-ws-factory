3704a445de827613c70d4e4a73986fded0cafd49
Steve Bezalel Iman Gustaman <13518018@std.stei.itb.ac.id>
2020-11-22 16:43:33 +0700

    Update to use env var

--- src/main/java/dao/DataSourceFactory.java
+++ src/main/java/dao/DataSourceFactory.java
@@ -4,15 +4,30 @@ import java.sql.*;
 
 public class DataSourceFactory {
 
-    private static final String HOST = "127.0.0.1";
-    private static final String IP = "3306";
-    private static final String DBNAME = "ws_factory";
-    private static final String USER = "root";
-    private static final String PASSWORD = "123";
+    private static String HOST = "127.0.0.1";
+    private static String IP = "3306";
+    private static String DBNAME = "ws_factory";
+    private static String USER = "root";
+    private static String PASSWORD = "123";
 
     private Connection conn = null;
 
+    private static void configureEnvironment(){
+        String ENV_HOST = System.getenv("WS_FACTORY_HOST");
+        String ENV_IP = System.getenv("WS_FACTORY_IP");
+        String ENV_DBNAME = System.getenv("WS_FACTORY_DBNAME");
+        String ENV_USER = System.getenv("WS_FACTORY_USER");
+        String ENV_PASSWORD = System.getenv("WS_FACTORY_PASSWORD");
+
+        HOST = ENV_HOST==null ? HOST : ENV_HOST;
+        IP = ENV_IP==null ? IP : ENV_IP;
+        DBNAME = ENV_DBNAME==null ? DBNAME : ENV_DBNAME;
+        USER = ENV_USER==null ? USER : ENV_USER;
+        PASSWORD = ENV_PASSWORD==null ? PASSWORD : ENV_PASSWORD;
+    }
+
     private DataSourceFactory() {
+        configureEnvironment();
         String url = String.format("jdbc:mysql://%s:%s/%s", HOST, IP, DBNAME);
         try {
             this.conn = DriverManager.getConnection(url, USER, PASSWORD);
