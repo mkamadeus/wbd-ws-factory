e1659a04b7dac1119544bee0a54edc50c9a547d9
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-11-23 09:47:19 +0700

    Hotfix IP varname to PORT

--- src/main/java/dao/DataSourceFactory.java
+++ src/main/java/dao/DataSourceFactory.java
@@ -14,13 +14,13 @@ public class DataSourceFactory {
 
     private static void configureEnvironment(){
         String ENV_HOST = System.getenv("WS_FACTORY_HOST");
-        String ENV_IP = System.getenv("WS_FACTORY_IP");
+        String ENV_PORT = System.getenv("WS_FACTORY_PORT");
         String ENV_DBNAME = System.getenv("WS_FACTORY_DBNAME");
         String ENV_USER = System.getenv("WS_FACTORY_USER");
         String ENV_PASSWORD = System.getenv("WS_FACTORY_PASSWORD");
 
         HOST = ENV_HOST==null ? HOST : ENV_HOST;
-        IP = ENV_IP==null ? IP : ENV_IP;
+        PORT = ENV_PORT==null ? PORT : ENV_PORT;
         DBNAME = ENV_DBNAME==null ? DBNAME : ENV_DBNAME;
         USER = ENV_USER==null ? USER : ENV_USER;
         PASSWORD = ENV_PASSWORD==null ? PASSWORD : ENV_PASSWORD;
@@ -28,7 +28,7 @@ public class DataSourceFactory {
 
     private DataSourceFactory() {
         configureEnvironment();
-        String url = String.format("jdbc:mysql://%s:%s/%s", HOST, IP, DBNAME);
+        String url = String.format("jdbc:mysql://%s:%s/%s", HOST, PORT, DBNAME);
         try {
             this.conn = DriverManager.getConnection(url, USER, PASSWORD);
         } catch (SQLException throwable) {
